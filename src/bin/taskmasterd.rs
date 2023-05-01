106207e08c91567f4b239a6d73c2dabd6409507f
Antoine Barthelemy <antoine@linux.com>
2023-05-01 22:35:53 +0200

    parse ini file

--- src/bin/taskmasterd.rs
+++ src/bin/taskmasterd.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use std::env;
+use std::fs;
 
 fn print_usage() {
     println!("Usage: /usr/bin/taskmasterd [options]");
@@ -96,10 +98,67 @@ fn parse_args() -> Args {
     args
 }
 
+#[derive(Debug)]
+struct IniConfig {
+    sections: HashMap<String, HashMap<String, String>>,
+}
+
+impl IniConfig {
+    fn new() -> Self {
+        IniConfig {
+            sections: HashMap::new(),
+        }
+    }
+
+    fn add_section(&mut self, section_name: String) {
+        self.sections.insert(section_name, HashMap::new());
+    }
+
+    fn add_key_value(&mut self, section_name: String, key: String, value: String) {
+        let section = self
+            .sections
+            .entry(section_name)
+            .or_insert_with(HashMap::new);
+        section.insert(key, value);
+    }
+}
+
+fn parse_ini_file(filename: &str) -> Result<IniConfig, std::io::Error> {
+    let contents = fs::read_to_string(filename)?;
+    let mut config = IniConfig::new();
+    let mut current_section = String::new();
+
+    for line in contents.lines() {
+        let line = line.trim();
+        if line.starts_with('#') || line.is_empty() {
+            continue;
+        }
+
+        if line.starts_with('[') && line.ends_with(']') {
+            current_section = line[1..line.len() - 1].to_string();
+            config.add_section(current_section.clone());
+        } else if let Some(idx) = line.find('=') {
+            let key = line[..idx].trim().to_string();
+            let value = line[idx + 1..].trim().to_string();
+            config.add_key_value(current_section.clone(), key, value);
+        } else {
+            eprintln!("Invalid line: {}", line);
+        }
+    }
+
+    Ok(config)
+}
+
 fn main() {
     let args = parse_args();
     if args.help {
         print_usage()
+    }
+    if let Some(filename) = args.configuration.as_deref() {
+        let config = parse_ini_file(filename).unwrap();
+        println!("{:#?}", config);
+    } else {
+        println!("No valide configuration file provided.");
     }
 
     std::process::exit(0)
