2e2b22270c903eb440b2fa4e1d72fe83bfdae67b
Antoine Barthelemy <antoine@linux.com>
2023-06-18 20:08:05 +0200

    configuration file order parsing file then c option

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,24 +1,54 @@
+use std::fs;
 use taskmaster::common::{recognize_file_format, FileFormat};
 use taskmaster::d::cli::{parse_args, print_usage, Args};
 use taskmaster::d::config::{parse_ini_file, parse_yq_file, Config};
 
 fn main() {
-    let args: Args = parse_args();
+    let mut args: Args = parse_args();
     println!("{:?}", args); // Debug
     if args.help {
         print_usage();
         std::process::exit(0);
     }
+
     if args.configuration.is_none() {
-        const LOOKAT: [&str; 6] = [
+        const LOOKAT: [&str; 24] = [
+            "../etc/supervisord.conf",          // Relative to the executable
+            "../supervisord.conf",              // Relative to the executable
+            "./supervisord.conf",               // Current working directory
+            "./etc/supervisord.conf",           // Current working directory
+            "/etc/supervisord.conf",            // Absolute path
+            "/etc/supervisor/supervisord.conf", // Supervisor 3.3.0 and above
             "../etc/taskmasterd.yaml",
             "../taskmasterd.yaml",
             "./taskmasterd.yaml",
             "./etc/taskmasterd.yaml",
-            "/etc/taskmasterdd.yaml",
+            "/etc/taskmasterd.yaml",
+            "/etc/taskmaster/taskmasterd.yaml",
+            "../etc/taskmasterd.json",
+            "../taskmasterd.json",
+            "./taskmasterd.json",
+            "./etc/taskmasterd.json",
+            "/etc/taskmasterd.json",
+            "/etc/taskmaster/taskmasterd.json",
+            "../etc/taskmasterd.conf",
+            "../taskmasterd.conf",
+            "./taskmasterd.conf",
+            "./etc/taskmasterd.conf",
+            "/etc/taskmasterd.conf",
             "/etc/taskmaster/taskmasterd.conf",
         ];
+
+        for file_path in LOOKAT.iter() {
+            if let Ok(metadata) = fs::metadata(file_path) {
+                if metadata.is_file() && metadata.permissions().readonly() {
+                    args.configuration = Some(file_path.to_string());
+                    break;
+                }
+            }
+        }
     }
+
     if let Some(filename) = args.configuration.as_deref() {
         match recognize_file_format(filename) {
             Some(FileFormat::Yaml) => {
