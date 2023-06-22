256ac09aec44a983fc1a7b80614becaaecadde1e
Antoine Barthelemy <antoine@linux.com>
2023-06-22 17:00:17 +0200

    implementing state machine

--- src/d/exec.rs
+++ src/d/exec.rs
@@ -11,34 +11,51 @@ enum ProcessState {
     UNKNOWN, // The process is in an unknown state (supervisord programming error).
 }
 
+struct ProcessInfo {
+    name: String,                             // Name of the process
+    state: ProcessState,                      // Current state of the process
+    pid: u32,                                 // Process ID
+    cpu_usage: f32,                           // CPU usage of the process
+    memory_usage: u64,                        // Memory usage of the process
+    start_time: DateTime<Utc>,                // Start time of the process
+    restart_count: u32,                       // Number of times the process has been restarted
+    last_restart_time: Option<DateTime<Utc>>, // Timestamp of the last process restart
+    exit_code: Option<u32>,                   // Exit code of the process if it has exited
+    supervisor: String,                       // Name or identifier of the process supervisor
+    stdout_log_file: Option<String>,          // Path to the stdout log file of the process
+    stderr_log_file: Option<String>,          // Path to the stderr log file of the process
+    environment: HashMap<String, String>,     // Environment variables specific to the process
+    program: Program,                         // Program elements
+}
+
 impl ProcessState {
-    fn transition(&self, config: &Config) -> Option<ProcessState> {
+    fn transition(&self, process: &ProcessInfo) -> Option<ProcessState> {
         match self {
             ProcessState::STOPPED => Some(ProcessState::STARTING), // should implement UNKNOW ?
             ProcessState::STARTING => {
-                if config.backoff {
+                if process.backoff {
                     Some(ProcessState::BACKOFF)
-                } else if config.running {
+                } else if process.running {
                     Some(ProcessState::RUNNING)
-                } else if config.stopping {
+                } else if process.stopping {
                     Some(ProcessState::STOPPING)
                 } else {
                     Some(ProcessState::UNKNOWN)
                 }
             }
             ProcessState::RUNNING => {
-                if config.stoppable {
+                if process.stoppable {
                     Some(ProcessState::STOPPING)
-                } else if config.exit {
+                } else if process.exit {
                     Some(ProcessState::EXITED)
                 } else {
                     Some(ProcessState::UNKNOWN)
                 }
             }
             ProcessState::BACKOFF => {
-                if config.starting {
+                if process.starting {
                     Some(ProcessState::STARTING)
-                } else if config.fatal {
+                } else if process.fatal {
                     Some(ProcessState::FATAL)
                 } else {
                     Some(ProcessState::UNKNOWN)
